f70b458bef2d048fd503d1892c8f7f5b6d276a59
Tanish Anand <tanishanand548@gmail.com>
2024-06-03 04:23:44 +0530

    Initialize Git

---
+++ #3-initialize-git.sh
@@ -0,0 +1,10 @@
+# Initialize Git
+# Once you have navigated to the correct folder, you can initialize Git on that folder
+
+$git init
+# o/p:- Initialized empty Git repository in /Users/user/myproject/.git/
+
+# You just created your first Git Repository!
+
+# Note: Git now knows that it should watch the folder you initiated it on.
+# Git creates a hidden folder to keep track of changes.
