343b8ad79fcba19d84f50de73fb71685623634d7
Tanish Anand <tanishanand548@gmail.com>
2024-06-03 04:23:13 +0530

    Creating Git Folder

---
+++ #2-create-git-folder.sh
@@ -0,0 +1,13 @@
+# Creating Git Folder
+# Now, let's create a new folder for our project
+
+$mkdir githubtutorial
+$cd githubtutorial
+
+# mkdir:- makes a new directory
+# cd:- changes the current working directory.
+
+# Now that we are in the correct directory. We can start by initializing Git!
+
+# Note: If you already have a folder/directory you would like to use for Git
+# Navigate to it in command line, or open it in your file explorer, right-click and select "Git Bash here"s
