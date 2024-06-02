cb786f2de10f2841956af5435ddfea4e8d636fcf
Tanish Anand <tanishanand548@gmail.com>
2024-06-03 04:22:47 +0530

    Configure Git

---
+++ #1-git-configure.sh
@@ -0,0 +1,9 @@
+# Configure Git
+# Now let Git know who you are. This is important for version control systems, as each Git commit uses this information
+
+$git config --global user.name "Tanish Anand"
+$git config --global user.email "xyz@gmail.com"
+
+
+# Note: Use global to set the username and e-mail for every repository on your computer.
+# If you want to set the username/e-mail for just the current repo, you can remove global.
